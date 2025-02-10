f56b753388d4dc7d5e17428cb83769f0e584e39e
Brandon <92910272+minju28@users.noreply.github.com>
2025-02-09 18:49:05 -0800

    Create CCC21 J3.py

---
+++ Practice Solutions/CCC21 J3.py
@@ -0,0 +1,14 @@
+#Full scoring submission 15/15
+
+direction = "" #define variables
+while True:
+    num = input()
+    firstTwo = int(num[0]) + int(num[1]) #adds the integer values of first two numbers
+    if num != "99999":
+        if firstTwo % 2 == 0 and firstTwo != 0: #if even and not zero direction is right
+            direction = "right"
+        elif firstTwo % 2 != 0: #if odd direction is left
+            direction = "left"
+        else: pass # if equal to zero, takes in last direciton used
+        print(direction + " " + num[2:]) #prints decoded version to console before looping again
+    else: break
