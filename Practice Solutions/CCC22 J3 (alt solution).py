14365a02fd45b2a9c8518e2a8d4a6f959ee5ff88
Brandon <92910272+minju28@users.noreply.github.com>
2025-02-14 18:22:22 -0800

    Update CCC22 J3 (alt solution).py

--- Practice Solutions/CCC22 J3 (alt solution).py
+++ Practice Solutions/CCC22 J3 (alt solution).py
@@ -9,13 +9,13 @@ for i in range(len(n)):
     if n[i] in alphabet or n[i] in symbols:
         result += n[i]
     if n[i] in numbers:
-        if i+1 < len(n) and n[i+1] in alphabet:
+        if i+1 < len(n) and n[i+1] in alphabet: #if the next character after a number is in alphabet, add the current index and a space
             result += n[i]
             result += " "
         else:
             result += n[i]
 
-x = result.split(' ')
+x = result.split() #split the string into a list at every space
 
 
 for y in range(len(x)):
