abfbf849a91fa428a802d069f0c9a50f8934f345
Brandon <92910272+minju28@users.noreply.github.com>
2025-02-15 00:01:10 -0800

    Update ccc24 j4.py

--- Practice Solutions/ccc24 j4.py
+++ Practice Solutions/ccc24 j4.py
@@ -6,26 +6,27 @@ Thinking process:
 2. Find possible troublesome keys by finding differences between both sets (two differenced sets, one for if in pressed but not in displayed and another for in displayed and not in pressed)
 3. If there is only 1 troublesome key, we know that there is no quiet key so we immediately know which keys are silly
 4. To differentiate between quiet and silly keys, we will test both possible troublesome keys by mimicking the effects of both the silly and quiet key onto pressed
-5. If not the first case, its the other.
+5. If not the first case, it is the other.
 
 """
 pressed = input() 
 displayed = input()
 
-pressed_set, displayed_set = set(pressed), set(displayed) #find unique characters
-difference_pressed, difference_displayed = pressed_set.difference(displayed_set), displayed_set.difference(pressed_set) # in pressed but not in displayed and in displayed but not in pressed
-difference_pressed, difference_displayed  = list(difference_pressed), list(difference_displayed) #turn into list for indexing
+#figure out possible troublesome keys
+pressed_set, displayed_set = set(pressed), set(displayed) 
+difference_pressed, difference_displayed = pressed_set.difference(displayed_set), displayed_set.difference(pressed_set)
+difference_pressed, difference_displayed  = list(difference_pressed), list(difference_displayed)
 
-if len(difference_pressed) == 1: #if there is only 1 troublesome key, that is the silly key. there is no quiet key
+if len(difference_pressed) == 1: #Case 1 if no quiet key: there will only be 1 possible troublesome key
     print(*difference_pressed, *difference_displayed)
     print(*'-')
 
-else: #now we need to figure out which of the two troublesome keys is silly and which is quiet
-    x = pressed.replace(difference_pressed[0], difference_displayed[0]) #to mimic silly key: have difference_pressed[0] as silly, and difference_pressed[1] as quiet
-    y = x.replace(difference_pressed[1], '')# to mimic quiet key: if correct this will equal displayed
+else: #Differentiate between silly and quiet key
+    x = pressed.replace(difference_pressed[0], difference_displayed[0]) #mimic silly key using first possible trouble key
+    y = x.replace(difference_pressed[1], '') #mimic quiet key with second possible trouble key
     if y == displayed:
-        print(difference_pressed[0], difference_displayed[0]) #first possible trouble key is the silly key
+        print(difference_pressed[0], difference_displayed[0]) 
         print(difference_pressed[1])
-    else:
-        print(difference_pressed[1], difference_displayed[0]) #if not, the trouble keys are filled, only other case
+    else: #flipped 
+        print(difference_pressed[1], difference_displayed[0]) 
         print(difference_pressed[0])
