82bc5bc2e481eae4d94f8a44e8486de530d269fd
Brandon <92910272+minju28@users.noreply.github.com>
2025-02-17 00:32:37 -0800

    Delete Practice Solutions/CCC24 J3.py

--- Practice Solutions/CCC24 J3.py
+++
@@ -1,8 +0,0 @@
-#updated solution 2025
-import sys
-input = sys.stdin.buffer.readline
-n = int(input())
-scores = [int(input()) for _ in range(n)]
-unique = list(set(scores))
-unique.sort(reverse=True)
-print(unique[2], scores.count(unique[2]))
